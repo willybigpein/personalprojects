80207b36dfea9dfcca47da2e58bb38a63590c03c
wewanchu <wewanchu@ualberta.ca>
2024-06-28 14:03:43 -0600

    fixed nested repositories

--- jobhunting/client/src/components/Header.jsx
+++ jobhunting/client/src/components/Header.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import CountUp from "react-countup";
 import { Link } from "react-router-dom";
 
 const Header = ({ data }) => {
@@ -8,7 +9,12 @@ const Header = ({ data }) => {
 			<h3 class="text-6xl">Applications</h3>
 			<div class="h-10 p-20 my-2 flex flex-col justify-center items-center  bg-gray-700 rounded-full">
 				<p>Total applied</p>
-				<div class="text-[50px] my-2">{data.length}</div>
+				<CountUp
+					end={data.length}
+					duration={5}
+					delay={1}
+					className="text-7xl font-extrabold"
+				/>
 			</div>
 			<Link
 				class="h-max rounded-lg font-extrabold shadow-black bg-green-600 shadow-lg hover:-translate-y-2 transition px-5 py-3 mx-3"
